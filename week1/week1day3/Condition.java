ae0900f37cdc657f07d40e3191e12483e15f6028
PurushothGangadharan <104833474+PurushothGangadharan@users.noreply.github.com>
2022-06-25 13:51:46 +0530

    Add files via upload

---
+++ week1/week1day3/Condition.java
@@ -0,0 +1,18 @@
+package week1day3;
+
+public class Condition {
+
+	public static void main(String[] args) {
+    int a=3;
+    int b=20;
+    if(a>b) {
+    System.out.println("EXECUTE");
+    }
+    else
+    {
+        System.out.println("NOT EXECUTE");
+    	
+    }
+	}
+
+}
